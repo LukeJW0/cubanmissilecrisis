d19cc0fd047c183acdf512902cac9a44ca577df9
Luke Withers <40157683+LukeJW0@users.noreply.github.com>
2022-01-07 09:14:23 -0600

    Update unit3websitejs.jsx

--- unit3websitejs.jsx
+++ unit3websitejs.jsx
@@ -1,3 +1,27 @@
-function changeText() {
-    document.getElementById('button').innerText = "Changed";
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function onStart() {
+    document.getElementById("thesispage").style.display = "none";
+    document.getElementById("firstpointpage").style.display = "none";
+    document.getElementById("secondpointpage").style.display = "none";
+    document.getElementById("thirdpointpage").style.display = "none";
+
+    var loadingscreen = document.getElementById("loading");
+    loadingscreen.style.display = "none";
+}
+
+var page = document.getElementById('homepage');
+
+function pageclicked(pageid) {
+    var buttonspage = document.getElementById("homepage");
+    buttonspage.style.display = "none";
+    page = document.getElementById(pageid);
+    page.style.display = "block";
+}
+
+function backpageclicked() {
+    page.style.display = "none";
+    document.getElementById("homepage").style.display = "block";
 }
